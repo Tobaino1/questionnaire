7019482d4e0f3c899284fbcaca240c82a8e2bacf
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2019-06-27 13:29:18 +0100

    creation of admin page

---
+++ admin_verify.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Oyekan Oluwatobi
+ * Date: 6/27/2019
+ * Time: 1:27 PM
+ */
+?>
+
+<html>
+<body>
+<p> admin login then the success page is going to be where data collected will be displayed</p>
+</body>
+</html>
