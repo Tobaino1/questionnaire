455586ac77ee3a8dec67857db55fc17d709b3c49
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2019-06-30 00:45:18 +0100

    creation of admin, questionnaire, admin verification

--- admin.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Oyekan Oluwatobi
- * Date: 6/26/2019
- * Time: 11:26 PM
- */
-?>
--- admin_verify.php
+++ admin_verify.php
@@ -12,14 +12,6 @@
 <p> admin login then the success page is going to be where data collected will be displayed</p>
 
 
-<p> admin login then the success page is going to be where data collected will be displayed</p>
-<p> admin login then the success page is going to be where data collected will be displayed</p>
-<p> admin login then the success page is going to be where data collected will be displayed</p>
-<p> admin login then the success page is going to be where data collected will be displayed</p>
-<p> admin login then the success page is going to be where data collected will be displayed</p>
-<p> admin login then the success page is going to be where data collected will be displayed</p>
-<p> admin login then the success page is going to be where data collected will be displayed</p>
-<p> admin login then the success page is going to be where data collected will be displayed</p>
 
 </body>
 </html>
