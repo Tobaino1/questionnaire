2193a2993cc4bfffa69375cd500dc1315c4f5a65
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2019-06-29 23:56:59 +0100

    creation of admin page

--- admin_verify.php
+++ admin_verify.php
@@ -10,5 +10,16 @@
 <html>
 <body>
 <p> admin login then the success page is going to be where data collected will be displayed</p>
+
+
+<p> admin login then the success page is going to be where data collected will be displayed</p>
+<p> admin login then the success page is going to be where data collected will be displayed</p>
+<p> admin login then the success page is going to be where data collected will be displayed</p>
+<p> admin login then the success page is going to be where data collected will be displayed</p>
+<p> admin login then the success page is going to be where data collected will be displayed</p>
+<p> admin login then the success page is going to be where data collected will be displayed</p>
+<p> admin login then the success page is going to be where data collected will be displayed</p>
+<p> admin login then the success page is going to be where data collected will be displayed</p>
+
 </body>
 </html>
