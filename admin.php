5eea749f70d359f9740e7d0e8b7abaaf405089b7
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2019-07-03 11:26:52 +0100

    admin page deployment

--- admin.php
+++ admin.php
@@ -24,86 +24,90 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <link rel="stylesheet" href="form.css">
     <script src="form.js"></script>
+
 </head>
 <body>
+<body>
 <!--<div class="container">-->
-    <div class="container form-top">
-        <div class="row">
-            <div class="card">
-                <div class="panel panel-danger">
-                    <div class="panel-body">
-                        <h4 style="color: blue" align="justify"><img src="images/lasulogo.PNG" alt="lasu"> Sociological
-                            Analysis of the Emerging e-Conflict among Youth (15years to 35 years) on Facebook
-                            and
-                            WhatsApp Platforms </h4>
-                        <!--<ul class="nav navbar-nav">-->
-                        <ul class="panel">
-                            <li><a href="index.html">HOME</a></li>
-                            <li><a href="questionnaire.php">QUESTIONNAIRE</a></li>
-                            <!--<li><a href="#">CONTACT / FEEDBACK</a></li>-->
-                            <li><a href="admin.php"> ADMIN </a></li>
-                        </ul>
+<div class="container form-top">
+    <div class="row">
+        <div class="card">
+            <div class="panel panel-danger">
+                <div class="panel-body">
+                    <h4 style="color: blue" align="justify"><img src="images/lasulogo.PNG" alt="lasu"> Sociological
+                        Analysis of the Emerging e-Conflict among Youth (15years to 35 years) on Facebook
+                        and
+                        WhatsApp Platforms </h4>
+                    <!--<ul class="nav navbar-nav">-->
+                    <ul class="panel">
+                        <li><a href="index.html">HOME</a></li>
+                        <li><a href="questionnaire.php">QUESTIONNAIRE</a></li>
+                        <!--<li><a href="#">CONTACT / FEEDBACK</a></li>-->
+                        <li><a href="admin.php"> ADMIN </a></li>
+                    </ul>
 
-                        <div class="col-lg-4">
-                            <div class="well contact-form-container">
+                    <div class="col-lg-4">
+                        <div class="well contact-form-container">
+
+                            <form action="#" method="post">
+                                <h4 style="color: blue">ADMINISTRATOR'S LOGIN PAGE</h4>
+
+                                <div class="form-item">
+                                    <span><i class="fa fa-user" style="color: green"></i></span>
+                                    <input type="text" name="user" required="required" placeholder="Username" autofocus
+                                           required></input>
+                                </div>
                                 <br>
-                                <form action="#" method="post">
-                                    <h4 style="color: blue">ADMINISTRATOR'S LOGIN PAGE</h4>
+                                <div class="form-item">
+                                    <span><i class="fa fa-key" style="color: green"></i></span>
+                                    <input type="password" name="pass" required="required" placeholder="Password"
+                                           required></input>
+                                </div>
+                                <br>
+                                <div class="button-panel">
+                                    <input type="submit" class="button" title="Log In" name="login"
+                                           value="Login"></input>
+                                </div>
+                            </form>
 
-                                    <div class="form-item">
-                                        <span><div class="fa fa-user" style="color: green"></div></span>
-                                        <input type="text" name="user" required="required" placeholder="Username"
-                                               autofocus required></input>
-                                    </div>
-                                    <br>
-                                    <div class="form-item">
-                                        <span><i class="fa fa-key" style="color: green"></i></span>
-                                        <input type="password" name="pass" required="required" placeholder="Password"
-                                               required></input>
-                                    </div>
-                                    <br>
-                                    <div class="button-panel">
-                                        <input type="submit" class="button" title="Log In" name="login"
-                                               value="Login"></input>
-                                    </div>
-                                </form>
-
-                                <!--<div class="form-wrapper">-->
-                                <br><br><br><br><br>
-                            </div>
+                            <!--<div class="form-wrapper">-->
+                            <br><br><br><br><br>
                         </div>
                     </div>
-                    <footer class="main-footer">
-                        <div class="container">
-                            <div class="pull-right hidden-xs">
-                                <b style="color: blue">Design by <a href="https://sparklegrid.tech/">Tobaino
-                                        (Sparklegrid)</a> </b>
-                            </div>
-                            <strong style="color: blue">Copyright &copy; 2019 Faculty of Social Science (All rights
-                                reserved)</strong>
-                        </div>
-                    </footer>
                 </div>
+
+                <br><br><br><br><br><br><br><br><br><br>
+                <footer class="main-footer">
+                    <div class="container">
+                        <div class="pull-right hidden-xs">
+                            <b style="color: blue">Design by <a href="https://sparklegrid.tech/">Tobaino
+                                    (Sparklegrid)</a> </b>
+                        </div>
+                        <strong style="color: blue">Copyright &copy; 2019 Faculty of Social Science (All rights
+                            reserved)</strong>
+                    </div>
+                </footer>
             </div>
         </div>
+    </div>
+    <?php
+    if (isset($_POST['login'])) {
+        $username = mysqli_real_escape_string($con, $_POST['user']);
+        $password = mysqli_real_escape_string($con, $_POST['pass']);
 
-        <?php
-        if (isset($_POST['login'])) {
-            $username = mysqli_real_escape_string($con, $_POST['user']);
-            $password = mysqli_real_escape_string($con, $_POST['pass']);
+        $query = mysqli_query($con, "SELECT * FROM users WHERE  password='$password' and username='$username'");
+        $row = mysqli_fetch_array($query);
+        $num_row = mysqli_num_rows($query);
 
-            $query = mysqli_query($con, "SELECT * FROM users WHERE  password='$password' and username='$username'");
-            $row = mysqli_fetch_array($query);
-            $num_row = mysqli_num_rows($query);
-
-            if ($num_row > 0) {
-                $_SESSION['user_id'] = $row['user_id'];
-                header('location:dashboard.php');
+        if ($num_row > 0) {
+            $_SESSION['user_id'] = $row['user_id'];
+            header('location:dashboard.php');
 //                            echo'tobi';
-            } else {
-                echo 'Invalid Username and Password Combination';
-            }
+        } else {
+            echo 'Invalid Username and Password Combination';
         }
-        ?>
+    }
+    ?>
+
 </body>
 </html>
