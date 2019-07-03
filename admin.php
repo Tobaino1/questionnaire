faac44c99260bbb2b56c74077eb63bae3d3d4216
Oyekan Oluwatobi <tobainocycle@yahoo.com>
2019-07-03 09:17:45 +0100

    update on admin, db con

--- admin.php
+++ admin.php
@@ -2,25 +2,21 @@
 /**
  * Created by PhpStorm.
  * User: Oyekan Oluwatobi
- * Date: 6/30/2019
- * Time: 9:53 PM
+ * Date: 3/3/2019
+ * Time: 8:38 AM
  */
 ?>
-
-
 <?php session_start(); ?>
-
 <?php include('dbcon.php'); ?>
-
-<!DOCTYPE html>
-<html lang="en">
+<!doctype html>
+<html lang="en" class="no-js">
 <head>
     <link rel="icon" href="images/lasulogo.PNG">
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Questionnaire</title>
+    <title>Admin Login</title>
     <!-- Latest compiled and minified CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <!-- Optional theme -->
@@ -30,101 +26,84 @@
     <script src="form.js"></script>
 </head>
 <body>
-<div class="container">
+<!--<div class="container">-->
     <div class="container form-top">
         <div class="row">
             <div class="card">
                 <div class="panel panel-danger">
                     <div class="panel-body">
                         <h4 style="color: blue" align="justify"><img src="images/lasulogo.PNG" alt="lasu"> Sociological
-                            Analysis of the Emerging e-Conflict among Youth (15years to 35 years) on Facebook and
+                            Analysis of the Emerging e-Conflict among Youth (15years to 35 years) on Facebook
+                            and
                             WhatsApp Platforms </h4>
                         <!--<ul class="nav navbar-nav">-->
                         <ul class="panel">
                             <li><a href="index.html">HOME</a></li>
                             <li><a href="questionnaire.php">QUESTIONNAIRE</a></li>
-                            <!--                            <li><a href="#">CONTACT / FEEDBACK</a></li>-->
+                            <!--<li><a href="#">CONTACT / FEEDBACK</a></li>-->
                             <li><a href="admin.php"> ADMIN </a></li>
                         </ul>
 
-                        <div id="content">
-                            <div class="box-section banner-section triggerAnimation animated" data-animate="rollIn">
+                        <div class="col-lg-4">
+                            <div class="well contact-form-container">
+                                <br>
+                                <form action="#" method="post">
+                                    <h4 style="color: blue">ADMINISTRATOR'S LOGIN PAGE</h4>
 
-                                <br><br>
-
-                                <div class="container">
-                                    <div class="row">
-                                        <div class="col-lg-4">
-                                            <div class="well contact-form-container">
-
-                                                <form action="#" method="post" style="color: blue">
-                                                    <h4 style="color: blue">ADMINISTRATOR'S LOGIN PAGE</h4>
-
-                                                    <div class="form-item">
-                                                        <span><i class="fa fa-user" style="color: green"></i></span>
-                                                        <input type="text" name="user" required="required"
-                                                               placeholder="Username" autofocus required></input>
-                                                    </div>
-                                                    <br>
-                                                    <div class="form-item">
-                                                        <span><i class="fa fa-key" style="color: green"></i></span>
-                                                        <input type="password" name="pass" required="required"
-                                                               placeholder="Password" required></input>
-                                                    </div>
-                                                    <br>
-                                                    <div class="button-panel">
-                                                        <input type="submit" class="button" title="Log In" name="login"
-                                                               value="Login"></input>
-                                                    </div>
-                                                </form>
-
-                                                <!--<div class="form-wrapper">-->
-
-                                            </div>
-                                        </div>
+                                    <div class="form-item">
+                                        <span><div class="fa fa-user" style="color: green"></div></span>
+                                        <input type="text" name="user" required="required" placeholder="Username"
+                                               autofocus required></input>
                                     </div>
-                                </div>
-                                <?php
-                                if (isset($_POST['login'])) {
-                                    $username = mysqli_real_escape_string($con, $_POST['user']);
-                                    $password = mysqli_real_escape_string($con, $_POST['pass']);
+                                    <br>
+                                    <div class="form-item">
+                                        <span><i class="fa fa-key" style="color: green"></i></span>
+                                        <input type="password" name="pass" required="required" placeholder="Password"
+                                               required></input>
+                                    </div>
+                                    <br>
+                                    <div class="button-panel">
+                                        <input type="submit" class="button" title="Log In" name="login"
+                                               value="Login"></input>
+                                    </div>
+                                </form>
 
-                                    $query = mysqli_query($con, "SELECT * FROM users WHERE  password='$password' and username='$username'");
-                                    $row = mysqli_fetch_array($query);
-                                    $num_row = mysqli_num_rows($query);
-
-                                    if ($num_row > 0) {
-                                        $_SESSION['user_id'] = $row['user_id'];
-                                        header('location:dashboard.php');
-//                            echo'tobi';
-                                    } else {
-                                        echo 'Invalid Username and Password Combination';
-                                    }
-                                }
-                                ?>
-
+                                <!--<div class="form-wrapper">-->
+                                <br><br><br><br><br>
                             </div>
-
-                            <br><br><br><br><br><br><br>
-                            <br><br><br><br><br>
-
-
-                            <!--footer section-->
-                            <footer class="main-footer">
-                                <div class="container">
-                                    <div class="pull-right hidden-xs">
-                                        <b>Design by <a href="https://sparklegrid.tech/">Tobaino (Sparklegrid)</a> </b>
-                                    </div>
-                                    <strong>Copyright &copy; 2019 Faculty of Social Science (All rights
-                                        reserved)</strong>
-                                </div>
-                            </footer>
-                            <!--end of footer section-->
                         </div>
                     </div>
+                    <footer class="main-footer">
+                        <div class="container">
+                            <div class="pull-right hidden-xs">
+                                <b style="color: blue">Design by <a href="https://sparklegrid.tech/">Tobaino
+                                        (Sparklegrid)</a> </b>
+                            </div>
+                            <strong style="color: blue">Copyright &copy; 2019 Faculty of Social Science (All rights
+                                reserved)</strong>
+                        </div>
+                    </footer>
                 </div>
             </div>
         </div>
-    </div>
+
+        <?php
+        if (isset($_POST['login'])) {
+            $username = mysqli_real_escape_string($con, $_POST['user']);
+            $password = mysqli_real_escape_string($con, $_POST['pass']);
+
+            $query = mysqli_query($con, "SELECT * FROM users WHERE  password='$password' and username='$username'");
+            $row = mysqli_fetch_array($query);
+            $num_row = mysqli_num_rows($query);
+
+            if ($num_row > 0) {
+                $_SESSION['user_id'] = $row['user_id'];
+                header('location:dashboard.php');
+//                            echo'tobi';
+            } else {
+                echo 'Invalid Username and Password Combination';
+            }
+        }
+        ?>
 </body>
 </html>
--- dbcon.php
+++ dbcon.php
@@ -5,14 +5,10 @@
  * Date: 6/30/2019
  * Time: 10:19 PM
  */ ?>
-
 <?php
 $con = mysqli_connect("localhost", "root", "", "research_work");
 // Check connection
 if (mysqli_connect_errno()) {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
-
 }
 ?>
-
-
